65cc2b98bc39896f6ed1a79c685b53a701f00d25
unknown <sifenbeyan@gmail.com>
2026-09-20 15:33:13 +0300

    frontend set-up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold color red  bg-blue-100">
+        SafeLink Ethiopia
+      </h1>
+    </div>
+  );
+}
+
+export default App;
